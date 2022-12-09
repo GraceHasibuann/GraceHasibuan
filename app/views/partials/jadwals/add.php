6b1a5e1a69e673d7b3fa73061bcec13f8f242d53
GraceHasibuann <gracehsb10@gmail.com>
2022-12-09 20:50:51 +0700

    pertama commit

---
+++ app/views/partials/jadwals/add.php
@@ -0,0 +1,141 @@
+<?php
+$comp_model = new SharedController;
+$page_element_id = "add-page-" . random_str();
+$current_page = $this->set_current_page_link();
+$csrf_token = Csrf::$token;
+$show_header = $this->show_header;
+$view_title = $this->view_title;
+$redirect_to = $this->redirect_to;
+?>
+<section class="page" id="<?php echo $page_element_id; ?>" data-page-type="add"  data-display-type="" data-page-url="<?php print_link($current_page); ?>">
+    <?php
+    if( $show_header == true ){
+    ?>
+    <div  class="bg-light p-3 mb-3">
+        <div class="container">
+            <div class="row ">
+                <div class="col ">
+                    <h4 class="record-title">Add New Jadwals</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php
+    }
+    ?>
+    <div  class="">
+        <div class="container">
+            <div class="row ">
+                <div class="col-md-7 comp-grid">
+                    <?php $this :: display_page_errors(); ?>
+                    <div  class="bg-light p-3 animated fadeIn page-content">
+                        <form id="jadwals-add-form" role="form" novalidate enctype="multipart/form-data" class="form page-form form-horizontal needs-validation" action="<?php print_link("jadwals/add?csrf_token=$csrf_token") ?>" method="post">
+                            <div>
+                                <div class="form-group ">
+                                    <div class="row">
+                                        <div class="col-sm-4">
+                                            <label class="control-label" for="kelas_id">Kelas Id <span class="text-danger">*</span></label>
+                                        </div>
+                                        <div class="col-sm-8">
+                                            <div class="">
+                                                <input id="ctrl-kelas_id"  value="<?php  echo $this->set_field_value('kelas_id',""); ?>" type="number" placeholder="Enter Kelas Id" step="1"  required="" name="kelas_id"  class="form-control " />
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-group ">
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <label class="control-label" for="mapel_id">Mapel Id <span class="text-danger">*</span></label>
+                                            </div>
+                                            <div class="col-sm-8">
+                                                <div class="">
+                                                    <input id="ctrl-mapel_id"  value="<?php  echo $this->set_field_value('mapel_id',""); ?>" type="number" placeholder="Enter Mapel Id" step="1"  required="" name="mapel_id"  class="form-control " />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="form-group ">
+                                            <div class="row">
+                                                <div class="col-sm-4">
+                                                    <label class="control-label" for="hari">Hari <span class="text-danger">*</span></label>
+                                                </div>
+                                                <div class="col-sm-8">
+                                                    <div class="">
+                                                        <input id="ctrl-hari"  value="<?php  echo $this->set_field_value('hari',""); ?>" type="text" placeholder="Enter Hari"  required="" name="hari"  class="form-control " />
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="form-group ">
+                                                <div class="row">
+                                                    <div class="col-sm-4">
+                                                        <label class="control-label" for="dari_jam">Dari Jam <span class="text-danger">*</span></label>
+                                                    </div>
+                                                    <div class="col-sm-8">
+                                                        <div class="input-group">
+                                                            <input id="ctrl-dari_jam" class="form-control datepicker  datepicker"  required="" value="<?php  echo $this->set_field_value('dari_jam',""); ?>" type="time" name="dari_jam" placeholder="Enter Dari Jam" data-enable-time="true" data-min-date="" data-max-date=""  data-alt-format="H:i" data-date-format="H:i:S" data-inline="false" data-no-calendar="true" data-mode="single" /> 
+                                                                <div class="input-group-append">
+                                                                    <span class="input-group-text"><i class="fa fa-clock"></i></span>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="form-group ">
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <label class="control-label" for="sampai_jam">Sampai Jam <span class="text-danger">*</span></label>
+                                                        </div>
+                                                        <div class="col-sm-8">
+                                                            <div class="input-group">
+                                                                <input id="ctrl-sampai_jam" class="form-control datepicker  datepicker"  required="" value="<?php  echo $this->set_field_value('sampai_jam',""); ?>" type="time" name="sampai_jam" placeholder="Enter Sampai Jam" data-enable-time="true" data-min-date="" data-max-date=""  data-alt-format="H:i" data-date-format="H:i:S" data-inline="false" data-no-calendar="true" data-mode="single" /> 
+                                                                    <div class="input-group-append">
+                                                                        <span class="input-group-text"><i class="fa fa-clock"></i></span>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="form-group ">
+                                                        <div class="row">
+                                                            <div class="col-sm-4">
+                                                                <label class="control-label" for="created_at">Created At <span class="text-danger">*</span></label>
+                                                            </div>
+                                                            <div class="col-sm-8">
+                                                                <div class="">
+                                                                    <input id="ctrl-created_at"  value="<?php  echo $this->set_field_value('created_at',""); ?>" type="text" placeholder="Enter Created At"  required="" name="created_at"  class="form-control " />
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group ">
+                                                            <div class="row">
+                                                                <div class="col-sm-4">
+                                                                    <label class="control-label" for="updated_at">Updated At <span class="text-danger">*</span></label>
+                                                                </div>
+                                                                <div class="col-sm-8">
+                                                                    <div class="input-group">
+                                                                        <input id="ctrl-updated_at" class="form-control datepicker  datepicker" required="" value="<?php  echo $this->set_field_value('updated_at',""); ?>" type="datetime"  name="updated_at" placeholder="Enter Updated At" data-enable-time="true" data-min-date="" data-max-date="" data-date-format="Y-m-d H:i:S" data-alt-format="F j, Y - H:i" data-inline="false" data-no-calendar="false" data-mode="single" /> 
+                                                                            <div class="input-group-append">
+                                                                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                                                                            </div>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="form-group form-submit-btn-holder text-center mt-3">
+                                                            <div class="form-ajax-status"></div>
+                                                            <button class="btn btn-primary" type="submit">
+                                                                Submit
+                                                                <i class="fa fa-send"></i>
+                                                            </button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </section>
---
+++ helpers/Menu.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'failed_jobs', 
+			'label' => 'Failed Jobs', 
+			'icon' => '<i class="fa fa-caret-left "></i>'
+		),
+		
+		array(
+			'path' => 'gurus', 
+			'label' => 'Gurus', 
+			'icon' => '<i class="fa fa-user "></i>'
+		),
+		
+		array(
+			'path' => 'jadwals', 
+			'label' => 'Jadwals', 
+			'icon' => '<i class="fa fa-th-large "></i>'
+		),
+		
+		array(
+			'path' => 'jawabans', 
+			'label' => 'Jawabans', 
+			'icon' => '<i class="fa fa-sticky-note-o "></i>'
+		),
+		
+		array(
+			'path' => 'jurusans', 
+			'label' => 'Jurusans', 
+			'icon' => '<i class="fa fa-paperclip "></i>'
+		),
+		
+		array(
+			'path' => 'kelas', 
+			'label' => 'Kelas', 
+			'icon' => '<i class="fa fa-building "></i>'
+		),
+		
+		array(
+			'path' => 'mapels', 
+			'label' => 'Mapels', 
+			'icon' => '<i class="fa fa-book "></i>'
+		),
+		
+		array(
+			'path' => 'migrations', 
+			'label' => 'Migrations', 
+			'icon' => '<i class="fa fa-cube "></i>'
+		),
+		
+		array(
+			'path' => 'password_resets', 
+			'label' => 'Password Resets', 
+			'icon' => '<i class="fa fa-upload "></i>'
+		),
+		
+		array(
+			'path' => 'personal_access_tokens', 
+			'label' => 'Personal Access Tokens', 
+			'icon' => '<i class="fa fa-file-archive-o "></i>'
+		),
+		
+		array(
+			'path' => 'siswas', 
+			'label' => 'Siswas', 
+			'icon' => '<i class="fa fa-female "></i>'
+		),
+		
+		array(
+			'path' => 'tugas', 
+			'label' => 'Tugas', 
+			'icon' => '<i class="fa fa-newspaper-o "></i>'
+		),
+		
+		array(
+			'path' => 'users', 
+			'label' => 'Users', 
+			'icon' => '<i class="fa fa-user-plus "></i>'
+		)
+	);
+		
+	
+	
+}
